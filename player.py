bac867d6b6592923e888b09a8043ea472080bcdb
Jalen Adams <leftysolara@gmail.com>
2014-08-14 09:31:58 -0400

    add add_card, remove_card, and set_wallet methods

--- player.py
+++ player.py
@@ -1,29 +1,39 @@
-from deck import Deck
-
 class Player(object):
-	"""Represents one poker player
+    """Represents one poker player
 
-	Attributes:
-		name:   player's name; will be shown when displaying player info
-		hand:   list of cards held by the player
-		wallet: amount of money player has
-	"""
+    Attributes:
+        name:   player's name; will be shown when displaying player info
+        hand:   list of cards held by the player
+        wallet: amount of money player has
+    """
 
-	def __init__(self, name="player", wallet=0):
-		self.name = name
-		self.hand = []
-		self.wallet = wallet
+    def __init__(self, name="player", wallet=0):
+        self.name = name
+        self.hand = []
+        self.wallet = wallet
 
-	def __str__(self):
-		return "Player: {}\nWallet: ${}".format(self.name, self.wallet)
+    def __str__(self):
+        return "Player: {}\nWallet: ${}".format(self.name, self.wallet)
 
-	def show_hand(self):
-		"""Displays the player's current hand"""
-		for card in self.hand:
-			print(card, end=" ")
-		print()
+    def set_wallet(self, amount):
+        """Sets player's wallet to given value"""
+        self.wallet = amount
 
-	def place_bet(bet=0):
-		"""Removes and returns money from wallet"""
-		self.wallet -= bet
-		return bet
+    def show_hand(self):
+        """Displays the player's current hand"""
+        for card in self.hand:
+            print(card, end=" ")
+        print()
+
+    def place_bet(self, bet=0):
+        """Removes and returns money from wallet"""
+        self.wallet -= bet
+        return bet
+
+    def add_card(self, card):
+        """Adds a card to player's hand"""
+        self.hand.append(card)
+
+    def pop_card(self):
+        """Removes and returns a card from player's hand"""
+        return self.hand.pop()
